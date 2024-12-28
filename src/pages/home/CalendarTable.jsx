5f04db16f2ce01b3bc7b4bbac6265e983fc338d2
bjh0499 <j9603h@gmail.com>
2024-12-28 23:40:06 +0900

    실제 날짜에 맞게 달력을 구성하는 함수 제작 중

--- src/pages/home/CalendarTable.jsx
+++ src/pages/home/CalendarTable.jsx
@@ -3,6 +3,13 @@ import React from "react";
 import CalendarRow from "./CalendarRow";
 
 export default function CalendarTable() {
+  let today = new Date();
+  let inputDay = new Date(today.getFullYear(), today.getMonth(), 1);
+  console.log(inputDay);
+  while (inputDay.getDay() > 0) {
+    inputDay = new Date(inputDay.valueOf() - 86400000);
+  }
+
   return (
     <div>
       <CalendarRow />
