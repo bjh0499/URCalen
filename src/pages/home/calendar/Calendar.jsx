03cb2a6e3b3f77f7e286ece2734898d454063e13
bjh0499 <j9603h@gmail.com>
2025-01-06 20:39:09 +0900

    달력 크기 조절 기능 구현 중

--- src/pages/home/calendar/Calendar.jsx
+++ src/pages/home/calendar/Calendar.jsx
@@ -4,9 +4,13 @@ import CalendarHeader from "./calendarHeader/CalendarHeader";
 import CalendarTable from "./calendarTable/CalendarTable";
 
 export default function Calendar({ monthSelector, holidays }) {
-  const [drag, setDrag] = useState(false);
-  let startX = -1;
-  let startY = -1;
+  const [start, setStart] = useState({
+    x: -1,
+    y: -1,
+    width: "384px",
+    height: "384px",
+  });
+
   const [calendarSize, setCalendarSize] = useState({
     width: "384px",
     height: "384px",
@@ -17,28 +21,27 @@ export default function Calendar({ monthSelector, holidays }) {
     let onePx = document.createElement("img");
     onePx.src = "/1px.png";
     e.dataTransfer.setDragImage(onePx, 0, 0);
-    startX = e.clientX;
-    startY = e.clientY;
-    console.log(e);
+    setStart({
+      x: e.clientX,
+      y: e.clientY,
+      width: calendarSize.width,
+      height: calendarSize.height,
+    });
   }
 
-  function handleDrag(e) {
-    //
-  }
-
-  function handleDragEnd(e) {
-    let widthString = calendarSize.width;
-    let heightString = calendarSize.height;
+  // BUG: dragover event를 쓸 경우, 실시간 크기 변경은 되지만, 포인터를 빨리 움직여 HTML 요소에서 벗어나면 크기 조절이 안 됨
+  // dragover는 HTML 전체에 두고, 각 div에 hover event를 넣어 해당 div에 커서가 있는 상태에서 드래그할 때 크기 조절이 되도록 변경 예정
+  function handleDragOver(e) {
     let finalX = Math.max(
-      Number(widthString.substring(0, widthString.length - 2)) +
+      Number(start.width.substring(0, start.width.length - 2)) +
         e.clientX -
-        startX,
+        start.x,
       384
     );
     let finalY = Math.max(
-      Number(widthString.substring(0, widthString.length - 2)) +
+      Number(start.height.substring(0, start.height.length - 2)) +
         e.clientY -
-        startY,
+        start.y,
       384
     );
 
@@ -54,8 +57,7 @@ export default function Calendar({ monthSelector, holidays }) {
       style={calendarSize}
       draggable={true}
       onDragStart={handleDragStart}
-      onDrag={handleDrag}
-      onDragEnd={handleDragEnd}
+      onDragOver={handleDragOver}
     >
       <CalendarHeader monthSelector={monthSelector} />
       <CalendarTable monthSelector={monthSelector} holidays={holidays} />
