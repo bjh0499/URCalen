0171436781bc8fec3b5affd918b2066423abdfc9
bjh0499 <j9603h@gmail.com>
2025-01-20 18:51:59 +0900

    우클릭 메뉴 창을 눌러도 해당 창이 사라지지 않게 event listener 구성

--- src/pages/home/calendar/calendarTable/CalendarMenu.jsx
+++ src/pages/home/calendar/calendarTable/CalendarMenu.jsx
@@ -1,8 +1,21 @@
 import React from "react";
 
 export default function CalendarMenu({ x, y }) {
+  const handleClick = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+  };
+
+  const handleRightClick = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div style={{ position: "absolute", left: `${x}px`, top: `${y}px` }}>
+    <div
+      style={{ position: "absolute", left: `${x}px`, top: `${y}px` }}
+      onClick={handleClick}
+      onContextMenu={handleRightClick}
+    >
       CalendarMenu
     </div>
   );
