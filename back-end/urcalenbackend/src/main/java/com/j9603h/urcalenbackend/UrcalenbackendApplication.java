284c755a01500fba8ba8de19d81007be0b75cfeb
bjh0499 <j9603h@gmail.com>
2025-02-07 19:13:30 +0900

    추후 Entity들이 공통적으로 사용할 시간 관련 속성을 정의

--- back-end/urcalenbackend/src/main/java/com/j9603h/urcalenbackend/UrcalenbackendApplication.java
+++ back-end/urcalenbackend/src/main/java/com/j9603h/urcalenbackend/UrcalenbackendApplication.java
@@ -2,7 +2,9 @@ package com.j9603h.urcalenbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class UrcalenbackendApplication {
 
