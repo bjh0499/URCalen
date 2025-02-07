d08565ea4c56261032c05f8390aed70ccfe7a242
bjh0499 <j9603h@gmail.com>
2025-02-07 20:49:05 +0900

    임시적으로 모든 요청에 대해 인증을 거치지 않게 설정

--- back-end/urcalenbackend/src/main/java/com/j9603h/urcalenbackend/global/config/SecurityConfig.java
+++ back-end/urcalenbackend/src/main/java/com/j9603h/urcalenbackend/global/config/SecurityConfig.java
@@ -2,9 +2,12 @@ package com.j9603h.urcalenbackend.global.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -12,5 +15,17 @@ public class SecurityConfig{
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll()
+                );
+
+        return http.build();
     }
 }
