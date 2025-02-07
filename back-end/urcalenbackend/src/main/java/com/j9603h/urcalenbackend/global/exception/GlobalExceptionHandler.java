6ddb1c5c2fd7f21186a0776f0e74d68d2e86b145
bjh0499 <j9603h@gmail.com>
2025-02-07 20:55:26 +0900

    HTTP request body가 올바르지 않아 발생할 수 있는 예외에 대한 처리 추가

--- back-end/urcalenbackend/src/main/java/com/j9603h/urcalenbackend/global/exception/GlobalExceptionHandler.java
+++ back-end/urcalenbackend/src/main/java/com/j9603h/urcalenbackend/global/exception/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.j9603h.urcalenbackend.global.exception;
 import com.j9603h.urcalenbackend.global.response.ApiResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -14,6 +15,13 @@ import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ApiResponse<Void>> handleHttpMessageNotReadable(HttpMessageNotReadableException ex) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(ApiResponse.error("요청 형식이 올바르지 않습니다.", "BAD_REQUEST"));
+    }
+
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<ApiResponse<Void>> handleUserNotFound(ResourceNotFoundException ex) {
         return ResponseEntity
