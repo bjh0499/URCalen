a6606ecae75d9fd01b3618798c7fc3c3f3004eb1
bjh0499 <j9603h@gmail.com>
2025-02-07 19:10:34 +0900

    공통적으로 사용될 HTTP 오류(400, 404, 405)에 대한 예외 처리 지정

---
+++ back-end/urcalenbackend/src/main/java/com/j9603h/urcalenbackend/global/exception/ResourceNotFoundException.java
@@ -0,0 +1,11 @@
+package com.j9603h.urcalenbackend.global.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+
+    public ResourceNotFoundException() {
+        super("요청한 리소스를 찾을 수 없습니다.");
+    }
+}
