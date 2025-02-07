173dc31440ad73998b31caa0eb1aa391ab177721
bjh0499 <j9603h@gmail.com>
2025-02-07 18:54:26 +0900

    기존 front-end를 하위 폴더로 이동, back-end 부분 추가

---
+++ back-end/urcalenbackend/src/main/java/com/j9603h/urcalenbackend/UrcalenbackendApplication.java
@@ -0,0 +1,13 @@
+package com.j9603h.urcalenbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrcalenbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrcalenbackendApplication.class, args);
+	}
+
+}
