1cb89aaedaffe9fd194bd5cb22d26ae55b738b73
bjh0499 <j9603h@gmail.com>
2025-04-08 18:17:37 +0900

    불필요한 Tailwind CSS Class 삭제

--- front-end/src/components/Header.jsx
+++ front-end/src/components/Header.jsx
@@ -1,6 +1,6 @@
 export default function Header() {
   return (
-    <div className="flex flex-row justify-center">
+    <div className="flex justify-center">
       <div className="text-5xl">URCalen</div>
     </div>
   );
