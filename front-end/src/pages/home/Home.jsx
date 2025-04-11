402b9d8fb03a2c45f49d6176d5a3859882779703
bjh0499 <j9603h@gmail.com>
2025-04-11 18:43:21 +0900

    달력 구조 변경: 페이지화된 구조에서 달력을 읽도록 변경

--- front-end/src/pages/home/Home.jsx
+++ front-end/src/pages/home/Home.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useSelector } from "react-redux";
 
 import Nav from "../../components/nav/Nav";
 import Calendar from "../../components/calendar/Calendar";
@@ -12,10 +13,10 @@ import SaveMenu from "../../components/menu/SaveMenu";
 
 export default function Home() {
   // TODO: 임시로 공휴일 정보를 해당 함수에서 지정하지만, 실제 배포 시에는 서버에서 받을 방침
-  let holidays = loadHolidays();
+  const holidays = loadHolidays();
 
   // TODO: 달 선택에 따라 변경되는 구성으로 개조 중이므로, 아래 내용은 year를 제외하고 삭제 예정
-  let today = new Date();
+  const today = new Date();
   const [monthSelector, setMonthSelector] = useState({
     year: today.getFullYear(),
     month: today.getMonth(),
@@ -32,8 +33,13 @@ export default function Home() {
   const [calendarSize, setCalendarSize] = useState({});
 
   const handleClick = () => {
-    setRightClickPosition(() => ({}));
-    setModalOption(() => ({}));
+    if (rightClickPosition.clickX !== undefined) {
+      setRightClickPosition(() => ({}));
+    }
+
+    if (modalOption.type !== undefined) {
+      setModalOption(() => ({}));
+    }
   };
 
   let modalContent = <></>;
@@ -62,6 +68,13 @@ export default function Home() {
     );
   }
 
+  const selectedMonth = useSelector((state) => state.selectedMonth.month);
+  const isFront = useSelector((state) => state.selectedMonth.front);
+  const calendarPageIdx = (selectedMonth << 1) + !isFront;
+  const calendarPage = useSelector(
+    (state) => state.calendarPages.calendarPages[calendarPageIdx]
+  );
+
   return (
     <div className="w-full h-full" onClick={handleClick}>
       <Nav
@@ -77,7 +90,7 @@ export default function Home() {
         calendarSize={calendarSize}
         setCalendarSize={setCalendarSize}
       />
-      {calendarKeyList.map((key) => (
+      {calendarPage.calendarKeyList.map((key) => (
         <Calendar
           key={key}
           calendarKey={key}
