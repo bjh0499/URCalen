86cd6b443cadca329c086afda5aa6ee9665b36dc
bjh0499 <j9603h@gmail.com>
2025-04-19 23:51:56 +0900

    불필요한 매개변수를 덜 제거하여 오류가 나던 부분 수정

--- front-end/src/pages/home/Home.jsx
+++ front-end/src/pages/home/Home.jsx
@@ -67,7 +67,6 @@ export default function Home() {
           calendarKey={key}
           monthSelector={monthSelector}
           holidays={holidays}
-          calendarOption={calendarOption}
           setRightClickPosition={setRightClickPosition}
         />
       ))}
