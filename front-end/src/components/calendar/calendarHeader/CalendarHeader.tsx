3336bc974ecf0803ab4c4758f56f12e4f3549aa2
bjh0499 <j9603h@gmail.com>
2025-07-12 14:41:47 +0900

    예기치 못한 부동소수점 연산 때문에 달력의 월 수가 잘못 나오던 걸 수정

--- front-end/src/components/calendar/calendarHeader/CalendarHeader.tsx
+++ front-end/src/components/calendar/calendarHeader/CalendarHeader.tsx
@@ -7,7 +7,7 @@ export default function CalendarHeader() {
   return (
     <div className="flex-center h-1/5 w-full">
       <div className="p-1 text-5xl">
-        {selectedYear + (selectedMonth - 1) / 12}.
+        {selectedYear + Math.floor((selectedMonth - 1) / 12)}.
         {((selectedMonth + 11) % 12) + 1}
       </div>
     </div>
