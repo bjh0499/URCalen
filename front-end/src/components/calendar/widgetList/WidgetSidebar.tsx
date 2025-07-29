71eb598ac624a837fb6632aba2a483d62b9e1c64
bjh0499 <j9603h@gmail.com>
2025-07-29 23:38:02 +0900

    widget 목록이 길어질 때를 위해 위/아래 버튼 추가

--- front-end/src/components/calendar/widgetList/WidgetSidebar.tsx
+++ front-end/src/components/calendar/widgetList/WidgetSidebar.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from "react";
 
 import WidgetListElement from "./WidgetListElement";
 
@@ -13,18 +14,38 @@ export default function WidgetSidebar({
   selectedPage,
   setRightClickPosition,
 }: WidgetSidebarInput) {
+  const [widgetStartIdx, setWidgetStartIdx] = useState<number>(0);
+
+  const handleWidgetListUpButton = (e) => {
+    if (widgetStartIdx > 0) {
+      setWidgetStartIdx(() => widgetStartIdx - 1);
+    }
+  };
+
+  const handleWidgetListDownButton = (e) => {
+    if (widgetStartIdx < selectedPage.calendarPage.widgetList.length - 10) {
+      setWidgetStartIdx(() => widgetStartIdx + 1);
+    }
+  };
+
   return (
     <aside className="flex-none" style={{ width: "100px" }}>
-      {selectedPage.calendarPage.widgetKeyList.map((key) => {
-        return (
-          <WidgetListElement
-            key={key}
-            idx={key}
-            selectedPage={selectedPage}
-            setRightClickPosition={setRightClickPosition}
-          />
-        );
+      <div onClick={handleWidgetListUpButton}>Up</div>
+      {selectedPage.calendarPage.widgetKeyList.map((key, idx) => {
+        if (idx >= widgetStartIdx && idx < widgetStartIdx + 10) {
+          return (
+            <WidgetListElement
+              key={key}
+              idx={key}
+              selectedPage={selectedPage}
+              setRightClickPosition={setRightClickPosition}
+            />
+          );
+        } else {
+          return <></>;
+        }
       })}
+      <div onClick={handleWidgetListDownButton}>Down</div>
     </aside>
   );
 }
